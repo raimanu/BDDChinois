99675f0e6d91be1d37f798bc2d07c19dc2d50f5e
raimanu <68045625+raimanu@users.noreply.github.com>
2024-06-12 11:00:28 +1100

    Rangement des dossiers

---
+++ Acupuncture/2_meridiens.sql
@@ -0,0 +1,20 @@
+INSERT INTO meridien (organe)
+VALUES ('cœur'),
+       ('poumon'),
+       ('foie'),
+       ('reins'),
+       ('vésicule biliaire'),
+       ('rate'),
+       ('utérus'),
+       ('gros intestin'),
+       ('petit intestin'),
+       ('poumons'),
+       ('foie (ou poumons)'),
+       ('estomac');
+
+INSERT INTO meridien (nom_meridien)
+VALUES ('six fu'),
+       ('zhèngzhòng (centre droit 正中)');
+
+select * from meridien;
+
